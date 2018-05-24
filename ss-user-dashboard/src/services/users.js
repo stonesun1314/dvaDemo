f2e2fb1b54a16cde22f463fef15438d2181fe4f0
stonesun1314 <user.email>
2018-05-24 14:08:54 +0800

    完成step6

--- ss-user-dashboard/src/services/users.js
+++ ss-user-dashboard/src/services/users.js
@@ -1,5 +1,6 @@
 import request from '../utils/request';
+import { PAGE_SZIE } from '../constants';
 
 export function fetch({ page = 1 }) {
-  return request(`/api/users?_page=${page}&_limit=5`);      // .._page、_limit前面加_作用是什么？
+  return request(`/api/users?_page=${page}&_limit=${PAGE_SZIE}`);      // .._page、_limit前面加_作用是什么？
 }
